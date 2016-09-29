3684131b77428a7bb081e43a521a9b4edca295ad
wangjiacheng <wangjiacheng@58ganji.com>
2016-09-29 22:35:13 +0800

    init

---
+++ app/src/main/java/com/zzuli/frescodelve/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zzuli.frescodelve;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
